8300cf69e72f4d0554d1f16d80ba877836156c57
lynarryan <lynarryan@gmail.com>
2021-03-30 23:35:08 -0400

    Resolved minor mistake with formatting/usage of the database

--- Tests/countrytest.py
+++ Tests/countrytest.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import sessionmaker
 # Generally follow CRUD (Create, Read, Update, and Delete) other functionality will arise as our program gets more complicated, at the very least stub in your test case when you add functionality
 # session.commit writes the actual changes to the database so that we can later use them otherwise they are stored in session memory
 
+
 class countryTest(unittest.TestCase):
 
     # setup and teardown methods do stuff like initailize the database connection and set up the session variable
@@ -22,10 +23,10 @@ class countryTest(unittest.TestCase):
         cls._session = Session()
 
     @classmethod
-    def tearDownClass(cls)
-    cls._session.close()
-    cls.db.close()
-    ##write an item to the database, check that it was added and remove it aftewards
+    def tearDownClass(cls):
+        cls._session.close()
+    # write an item to the database, check that it was added and remove it aftewards
+
     def test_addCountry(self):
         # Initialize items to be used in testing
         testCountry = Country(countryname="test_CountryName")
@@ -43,16 +44,16 @@ class countryTest(unittest.TestCase):
         # clean up the database by removing test item we only need to pass the id as it is the primary key
         query.filter(Country.countryid == testCountry.countryid).delete()
         self._session.commit()
-    
-    ##write an item to the database, delete it, verify that it is not in the database
+
+    # write an item to the database, delete it, verify that it is not in the database
     def test_deleteCountry(self):
         raise NotImplementedError("error")
-    
-    ## write an item to the database, retrieve it, modify it, write it back to the database, ensure that hte new version is in the database
+
+    # write an item to the database, retrieve it, modify it, write it back to the database, ensure that hte new version is in the database
     def test_updateCountry(self):
         raise NotImplementedError("error")
-    
-    ## write a set of items to the database, retrieve them, verify that the contents are expected and the count is expected
+
+    # write a set of items to the database, retrieve them, verify that the contents are expected and the count is expected
     def test_listCountries(self):
         raise NotImplementedError("error")
 
